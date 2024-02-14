0be658b40a0fbbd7352c8bc9f46de0e2870d16f5
arnold-wafula <arnold.wafula0@gmail.com>
2024-02-14 16:19:03 +0300

    changed redirection function

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -6,9 +6,7 @@ use Illuminate\Http\Request;
 
 class DashboardController extends Controller
 {
-  
-
     public function dashboard() {
-        return view('dashboard');
+        return redirect('dashboard');
     }
 }
