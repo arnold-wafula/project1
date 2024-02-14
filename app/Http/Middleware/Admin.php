ffcce33bb256bd278e2af6eacc8620da311f24ab
arnold-wafula <arnold.wafula0@gmail.com>
2024-02-14 14:31:17 +0300

    Removed backslash in login route

--- app/Http/Middleware/Admin.php
+++ app/Http/Middleware/Admin.php
@@ -18,7 +18,7 @@ class Admin
     public function handle(Request $request, Closure $next)
     {
       if (!Auth::check()){
-        return redirect('/login');
+        return redirect()->route('login');
       }
       if (Auth::user()->role == 1) {
         return $next($request);
