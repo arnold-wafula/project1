c9ef41cda545e693ceba0ad4903d2ae13e038f34
arnold-wafula <arnold.wafula0@gmail.com>
2024-02-15 12:55:19 +0300

    Changed Logistic middleware class name

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -65,6 +65,6 @@ class Kernel extends HttpKernel
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
         'admin' => \App\Http\Middleware\Admin::class,
-        'logistic' => \App\Http\Middleware\LogisticUser::class,
+        'logistic' => \App\Http\Middleware\Logistic::class,
     ];
 }
